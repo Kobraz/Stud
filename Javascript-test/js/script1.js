449843017aa47150785959d9ae811641e7479b65
Kobraz <roar.f.hanssen@gmail.com>
2023-01-28 22:13:24 +0100

    jjjjj

--- Javascript-test/js/script1.js
+++ Javascript-test/js/script1.js
@@ -1,11 +1,6 @@
-let firstName = "Roar";
-
-
-console.log(firstName);
-
 
 const pi = 3.141592653589;
-let radius = 12,25;
+let radius = 1225;
 let omkrets = 2 * radius * pi;
 
 console.log("Omkretsen er " + omkrets);
